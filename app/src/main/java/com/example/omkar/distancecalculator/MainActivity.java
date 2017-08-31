38d5dfda60268d96b0baf6bc8e887094d1aeda96
Omkar Joshi <onjoshi@ncsu.edu>
2017-08-31 02:36:26 -0400

    Handled marshmallow update for permissions

--- app/src/main/java/com/example/omkar/distancecalculator/MainActivity.java
+++ app/src/main/java/com/example/omkar/distancecalculator/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
@@ -24,23 +25,24 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    // start variable to send the additional variable in the first request
     boolean start;
+    // random integer which acts as a constant. This same constant is used to retrieve when user responds with the request.
+    final static int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 45;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,17 +50,22 @@ public class MainActivity extends AppCompatActivity {
         start = true;
         final Button startButton = (Button)  findViewById(R.id.btnStart);
 
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+
         startButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if (startButton.getText().equals("START")){
+                if (startButton.getText().toString().equalsIgnoreCase("START")){
                     startButton.setText("STOP");
                     final EditText host = (EditText)  findViewById(R.id.editTextHost);
                     final EditText username = (EditText)  findViewById(R.id.editTextUsername);
-                    if(start){
-                        startLocationTracker(host.getText().toString(), username.getText().toString());
-                    }
-                    else
-                        startLocationTracker(host.getText().toString(), username.getText().toString());
+                    startLocationTracker(host.getText().toString(), username.getText().toString());
                 }else{
                     startButton.setText("START");
                     stopLocationTracker();
@@ -68,16 +75,31 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i("Main Activity", "Permission granted for retrieving the FINE LOCATION");
+                } else {
+                    //Permission-denied
+                    Log.w("Main Activity", "Permission not granted for retrieving the FINE LOCATION");
+                    Toast.makeText(this, "Permission not granted for retrieving the FINE LOCATION", Toast.LENGTH_SHORT).show();
+                    final Button startButton = (Button)  findViewById(R.id.btnStart);
+                    startButton.setEnabled(false);
+                }
+            }
+        }
+    }
 
-    Timer timer;
-    int delay = 2000; // delay for 2 sec.
-    int period = 15000; // repeat every 5 secs.
+    ScheduledThreadPoolExecutor exec; // Better than timer and timertask.
+    int period = 5000; // repeat every 5 secs. Period is the interval between multiple timer calls.
 
     void startLocationTracker(final String host, final String username){
-        timer = new Timer();
-        Log.i("Main Activity", "Started");
-        timer.scheduleAtFixedRate(new TimerTask(){
+        exec = new ScheduledThreadPoolExecutor(1); //5 denotes the number of threads in pool for parallel processing.
+        Log.i("Main Activity", "Timer Start.");
+        exec.scheduleAtFixedRate(new Runnable(){
             @Override
             public void run() {
                 {
@@ -105,10 +127,11 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             }
-        }, delay, period);
+        },0, period, TimeUnit.SECONDS);
     }
 
     double totalDistance = 0.0;
+
     void sendPostRequestToServer(final double latitude, final double longitude, String username, String host){
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
@@ -124,6 +147,8 @@ public class MainActivity extends AppCompatActivity {
         if (start){
             params.put("start", "true");
             start = false;
+        }else{
+            params.put("start", "false");
         }
         JsonObjectRequest request_json = new JsonObjectRequest(Request.Method.POST, URL, new JSONObject(params),
                 new Response.Listener<JSONObject>() {
@@ -162,9 +187,8 @@ public class MainActivity extends AppCompatActivity {
     void stopLocationTracker(){
 //        Toast.makeText(this, "Stopped", Toast.LENGTH_SHORT).show();
         Log.i("Main Activity", "Stoppped");
-        if (timer != null){
-            timer.cancel();
-            timer.purge();
+        if (exec != null){
+            exec.shutdown();
         }
         Toast.makeText(this, "Total Distance: "+totalDistance, Toast.LENGTH_SHORT).show();
     }
@@ -172,9 +196,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (timer != null){
-            timer.cancel();
-            timer.purge();
+        if (exec != null){
+            exec.shutdown();
         }
     }
 }
